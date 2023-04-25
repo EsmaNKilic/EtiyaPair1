2552a459a37a089d2aca1495261554a29af78feb
Esma <iyidost1999@hotmail.com>
2023-04-25 09:18:32 +0300

    entities

---
+++ src/main/java/com/etiya/ecommercedemopair1/entities/concretes/IndivicualCustomer.java
@@ -0,0 +1,26 @@
+package com.etiya.ecommercedemopair1.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="indivicual_customers")
+public class IndivicualCustomer {
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name="identity_no")
+    private String identityNo;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "id")
+    private Customer customer;
+
+}
---
+++ src/main/java/com/etiya/ecommercedemopair1/entities/concretes/Order.java
@@ -0,0 +1,38 @@
+package com.etiya.ecommercedemopair1.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="status")
+    private boolean status;
+
+    @OneToMany(mappedBy = "order")
+    private List<ProductOrder> productOrders;
+
+    @ManyToOne()
+    @JoinColumn(name="shipping_id")
+    private Shipping shipping;
+
+    @ManyToOne()
+    @JoinColumn(name="customer_id")
+    private Customer customer;
+
+}
