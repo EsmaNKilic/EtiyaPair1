fe1bd5da878ea4674f29dd617e1c487fbfe53c29
Esma <iyidost1999@hotmail.com>
2023-04-28 18:44:42 +0300

    unitTest

---
+++ src/test/java/com/etiya/ecommercedemopair1/business/concrate/CategoryManagerTest.java
@@ -0,0 +1,28 @@
+package com.etiya.ecommercedemopair1.business.concrate;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryManagerTest {
+
+    @BeforeEach
+    void setUp() {
+        // Her test öncesi çalıştırılacak alan
+    }
+
+    @AfterEach
+    void tearDown() {
+        // Her test sonrası çalıştırılacak alan
+    }
+
+    @Test
+    void add() {
+        // 3A Principle ↓
+        // Arrange => Test yapılacak ortamı hazırlar, mock objelerin oluşturulması, bağımlılıkların yüklenmesi
+        // Act(Action) => CategoryManager.add(); test edeceğin metodun işleme alınması
+        // Assert(Beklenti) => Expected durum ile Real durum karşılaştırmasının yapıldığı nokta => categoryManager.add().throwsException() ? boolean
+    }
+}
